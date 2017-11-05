55c963bc159544035c0b6b40d244e3ee04d1a53d
DaveDLeague <dave.dunn@jointheleague.org>
2017-11-05 14:34:54 -0800

    Add files via upload

---
+++ ExceptionsPractice.java
@@ -0,0 +1,24 @@
+package test;
+
+public class ExceptionsPractice {
+	//1. create a main method to test the other methods
+	
+	//2. create a method that takes a string and checks if it matches a password of your choice. 
+	//     throw a SecurityException if the string does not match the password
+	
+	//3. create a method that takes in two ints and a String and returns an int. The string will be 
+	//    either "add" "multiply" or "subtract." Return the proper result with the two ints. 
+	//    throw an UnsupportedOperation exception if the string does not match a proper operation
+	
+	//4. create a mehtod that takes an ArrayList of strings and an int. The method will then print
+	//     all the objects from the list starting from the int. Throw an IndexOutOfBoundsException of
+	//     the list does not contain that element
+	
+	//5. create a method that takes 2 ints and an ArrayList. This method will print the range of elements
+	//    provided by the two ints. Throw an NumberFormatException if the numbers are not in ascending order.
+	
+	//6. Create a class called Vector2D that has two member floats called x and y.
+	//     Create a method called normalize that takes in a Vector2D and returns a Vector2D. 
+	//     The method should return the normal of the vector. Throw a DivideByZeroException if the
+	//     x and y of the passed in vector are both 0.
+}
